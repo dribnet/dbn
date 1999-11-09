4702a29f8f2c54921845c7f0962858ef86d2f4b2
Ben Fry <fry@mit.edu>
1999-11-09 13:30:02 +0000

    minor change to cab.bat

--- app/cab.bat
+++ app/cab.bat
@@ -2,4 +2,5 @@
 cd classes2
 cabarc -r -p N dbn.cab *.class *.dbn jscheme\*.class org\python\compiler\*.class org\python\core\*.class org\python\modules\*.class org\python\parser\*.class org\python\rmi\*.class org\python\util\*.class
 cd ..
-signcode -j javasign.dll -jp LOW -spc ..\cert\acgcert.spc -v ..\cert\acgkey.pvk classes2\dbn.cab
+rem signcode -j javasign.dll -jp LOW -spc ..\cert\acgcert.spc -v ..\cert\acgkey.pvk classes2\dbn.cab
+signcode -n "Design By Numbers" -j javasign.dll -jp LOW -spc ..\cert\acgcert.spc -v ..\cert\acgkey.pvk classes2\dbn.cab
