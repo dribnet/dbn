8d479e40a427e36fefa507420b2c57be28c4690f
Ben Fry <fry@mit.edu>
1999-11-15 03:54:02 +0000

    rm -f instead of rm

--- app/cricket/dist.bat
+++ app/cricket/dist.bat
@@ -3,8 +3,8 @@
 rem --- you must first call make.bat
 rem --- creates a copy of cricket dbn distro as a zip file
 
-rm readme.txt
-rm dbn.zip
+rm -f readme.txt
+rm -f dbn.zip
 
 cp ..\readme.txt .\readme.txt
 zip -rq dbn.zip run.bat readme.txt bin\*.dll bin\*.exe lib\*.properties lib\*.jar lib\security\java.security 
