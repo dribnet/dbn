0944f2e3cb740ac21d0c2c1543dbd31ff466cf4c
Ben Fry <fry@mit.edu>
2000-04-22 02:21:39 +0000

    minor updates to kvm work

--- app/kvm/make.bat
+++ app/kvm/make.bat
@@ -3,7 +3,6 @@
 rem -- this is an attempt at using the kvm for dbn
 
 rm -f classes\*.class
-rem rm -f classes\dbn.jar
 
 set ME=kvm
 
@@ -11,8 +10,8 @@ set SAVED_CLASSPATH=%CLASSPATH%
 set CLASSPATH=.\%ME%\kvm.zip
 
 cd ..
-rem buzz.pl "jikes +1.0 +D -nowarn -d %ME%\classes" -dKVM *.java kvm\*.java
-buzz.pl "sj -nowarn -d %ME%\classes" -dKVM *.java kvm\*.java
+buzz.pl "jikes +1.0 +D -nowarn -d %ME%\classes" -dKVM *.java kvm\*.java
+rem buzz.pl "sj -nowarn -d %ME%\classes" -dKVM *.java kvm\*.java
 
 cd %ME%\classes
 rem zip -0q dbn.jar *.class *.dbn
