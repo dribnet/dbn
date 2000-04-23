127c64be07b36da375eefb3eb7aa9c13b1703e84
Ben Fry <fry@mit.edu>
2000-04-23 18:59:14 +0000

    using lib\ instead of .\lib\

--- app/experimental/run.bat
+++ app/experimental/run.bat
@@ -1,2 +1,2 @@
 @echo off
-.\bin\jre -nojit -Dpython.home=. -mx48m -cp .\lib;lib\dbn.jar;lib\jpython.jar;lib\qtjava.zip Experimental
+.\bin\jre -nojit -Dpython.home=. -mx48m -cp lib\dbn.jar;lib\jpython.jar;lib\qtjava.zip;lib\javac.jar Experimental
